05d2a36ef0356422139e5a77ad529171b40d2521
Mr. Wayne <70208852+a4arpon@users.noreply.github.com>
2023-05-02 08:29:51 +0000

    Recipe Added

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send(data)
 });
-
+// chef Filter By id
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedData = data.find(n => n._id === id);
+    res.send(selectedData)
+})
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
